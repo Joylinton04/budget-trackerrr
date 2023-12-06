7468e6d8ea0e9f88772fb37f0dc0c5f046fbba83
JoyLinton04 <adujoy05@gmail.com>
2023-12-06 22:40:46 +0000

    Added redux store and reducers

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const DataExpense = [
+    {
+      "id": "Food/drinks",
+      "label": "Food/drinks",
+      "value": 3000,
+      "color": "hsl(289, 70%, 50%)"
+    },
+    {
+      "id": "vacation",
+      "label": "vacation",
+      "value": 5000,
+      "color": "hsl(358, 70%, 50%)"
+    },
+    {
+      "id": "transport",
+      "label": "transport",
+      "value": 9000,
+      "color": "hsl(224, 70%, 50%)"
+    },
+    {
+      "id": "Hobbies",
+      "label": "Hobbies",
+      "value": 5000,
+      "color": "hsl(39, 70%, 50%)"
+    },
+    {
+      "id": "Rent Apartment",
+      "label": "Rent Apartment",
+      "value": 1000,
+      "color": "hsl(359, 70%, 50%)"
+    },
+    {
+      "id": "Others",
+      "label": "Othes",
+      "value": 2000,
+      "color": "hsl(359, 70%, 50%)"
+    },
+  ]
